41a6eecea87276177745bb77d2e4b236a7c1a610
Apple inside <artur@MacBook-Pro-ARTUR.local>
2020-10-22 21:53:14 +0300

    add editor and button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,40 +1,15 @@
 import React, { Component } from "react";
 import "../App.css";
-import Username from "../components/Username";
 import { observer } from "mobx-react";
 import { withStore } from "../services/store";
+import Editor from "../components/Editor";
 
 @withStore
 @observer
 class Home extends Component {
-  toggleTitle = () => {
-    const { store } = this.props;
-    if (store.title === "Coding is Love") {
-      store.setTitle("Mobx React Context");
-    } else {
-      store.setTitle("Coding is Love");
-    }
-  };
-
   render() {
-    const { store } = this.props;
     return (
-      <div className="App">
-        <header className="App-header">
-          <a
-            className="App-link"
-            href="https://codingislove.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {store.title}
-          </a>
-          <button onClick={this.toggleTitle} style={{ margin: 20 }}>
-            Toggle title
-          </button>
-          <Username />
-        </header>
-      </div>
+      <Editor/>
     );
   }
 }
